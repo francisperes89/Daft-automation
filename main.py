9b987d4601a856e7175a30f386a59d2d2df72580
francisperes89 <francis.po@hotmail.com>
2023-12-03 16:59:21 +0000

    Added Select function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
 import time
 # Tkinter GUI for the data needed to start scraping
 # Global variables
@@ -23,12 +24,13 @@ def get_data():
 
 
 def scraping_data():
+    global rent_value, num_rooms
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_experimental_option("detach", True)
     driver = webdriver.Chrome(options=chrome_options)
     driver.get("https://www.daft.ie/homes-to-rent")
     driver.maximize_window()
-    time.sleep(4)
+    time.sleep(3)
     cookie_accept = driver.find_element(By.XPATH, "//*[@id='didomi-notice-agree-button']")
     cookie_accept.click()
     time.sleep(0.7)
@@ -36,9 +38,41 @@ def scraping_data():
     location_input.send_keys(location)
     time.sleep(0.7)
     location_input.send_keys(Keys.ENTER)
-    time.sleep(0.7)
+    time.sleep(2)
     price_button = driver.find_element(By.XPATH, "//*[@id='__next']/main/div[1]/div/div[2]/div[1]/div[1]/button")
     price_button.click()
+    time.sleep(2)
+    selection_price_up_to = Select(driver.find_element(By.XPATH, "//*[@id='rentalPriceTo']"))
+    selection_price_up_to.select_by_value(rent_value)
+    time.sleep(1)
+    rooms_button = driver.find_element(By.XPATH, "//*[@id='__next']/main/div[1]/div/div[2]/div[2]/div[1]/button")
+    rooms_button.click()
+    time.sleep(1)
+    selection_rooms_up_to = Select(driver.find_element(By.XPATH, "//*[@id='numBedsTo']"))
+    selection_rooms_up_to.select_by_value(num_rooms)
+    list_elements = driver.find_elements(By.CSS_SELECTOR, "ul li a div div .fKxuMi")
+    time.sleep(2)
+    list_of_urls = []
+    for i in range(len(list_elements)):
+        current_element = driver.find_elements(By.CSS_SELECTOR, "ul li a div div .fKxuMi")[i]
+        current_element.click()
+        url = driver.current_url
+        list_of_urls.append(url)
+        print(url)
+        address_element = driver.find_element(By.XPATH, "//*[@id='__next']/main/div[3]/div[1]/div[1]/div/div[2]/h1")
+        address = address_element.text
+        print(address)
+        rent_element = driver.find_element(By.XPATH, "//*[@id='__next']/main/div[3]/div[1]/div[1]/div/div[2]/div[1]/h2")
+        rent = rent_element.text
+        print(rent)
+        time.sleep(1.5)
+        driver.back()
+        time.sleep(2)
+
+
+
+
+
 
     # Here you can do whatever you want with the data, like printing it to the console
     print("Location:", location)
